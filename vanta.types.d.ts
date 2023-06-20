7072fa8c2b87f43ebac0be0ada33b63702d2591c
Yağız Işkırık <y_iskirik_10@hotmail.com>
2023-06-20 20:31:36 +0300

    feat(frontent): :sparkles: Add moving background back to the page

---
+++ vanta.types.d.ts
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Yağız Işkırık
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+declare module 'vanta/dist/vanta.net.min' {
+  const net: any; // eslint-disable-line
+  export default net;
+}
