b98d2e95f54f78fc543d38083bf824e373b295ca
Yağız Işkırık <y_iskirik_10@hotmail.com>
2023-06-24 12:31:33 +0300

    feat(frontend, backend): :wrench: Edit logo files and info

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,21 +1,15 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-// !STARTERCONF Change these default meta
 const defaultMeta = {
   title: 'h4ppy Wifi Interface',
   siteName: 'h4ppy Wifi Interface',
   description:
     'Another Wi-Fi penetration testing multi-tool for wide variety of applications',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'http://localhost:3000',
   type: 'website',
   robots: 'follow, index',
-  /**
-   * No need to be filled, will be populated with openGraph function
-   * If you wish to use a normal image, just specify the path below
-   */
-  image: 'http://localhost:3000/images/large-og.png',
+  image: 'http://localhost:3000/images/logo.png',
 };
 
 type SeoProps = {
@@ -32,16 +26,6 @@ export default function Seo(props: SeoProps) {
   meta['title'] = props.templateTitle
     ? `${props.templateTitle} | ${meta.siteName}`
     : meta.title;
-
-  // Use siteName if there is templateTitle
-  // but show full title if there is none
-  // !STARTERCONF Follow config for opengraph, by deploying one on https://github.com/theodorusclarence/og
-  // ? Uncomment code below if you want to use default open graph
-  // meta['image'] = openGraph({
-  //   description: meta.description,
-  //   siteName: props.templateTitle ? meta.siteName : meta.title,
-  //   templateTitle: props.templateTitle,
-  // });
 
   return (
     <Head>
@@ -77,15 +61,13 @@ export default function Seo(props: SeoProps) {
       {favicons.map((linkProps) => (
         <link key={linkProps.href} {...linkProps} />
       ))}
-      <meta name='msapplication-TileColor' content='#000000' />
+      <meta name='msapplication-TileColor' content='#121417' />
       <meta name='msapplication-config' content='/favicon/browserconfig.xml' />
-      <meta name='theme-color' content='#000000' />
+      <meta name='theme-color' content='#121417' />
     </Head>
   );
 }
 
-// !STARTERCONF this is the default favicon, you can generate your own from https://realfavicongenerator.net/
-// ! then replace the whole /public/favicon folder and favicon.ico
 const favicons: Array<React.ComponentPropsWithoutRef<'link'>> = [
   {
     rel: 'apple-touch-icon',
@@ -108,7 +90,7 @@ const favicons: Array<React.ComponentPropsWithoutRef<'link'>> = [
   {
     rel: 'mask-icon',
     href: '/favicon/safari-pinned-tab.svg',
-    color: '#00e887',
+    color: '#f5c265',
   },
   { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
 ];
